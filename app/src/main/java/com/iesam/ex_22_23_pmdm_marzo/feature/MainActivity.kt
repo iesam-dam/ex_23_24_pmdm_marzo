5563fb0f18bd2cc887d7d4bef7fdf6a2165d92ef
diegogd04 <diego2612004@gmail.com>
2024-02-23 18:35:15 +0100

    Creación de vistas e implementación de Hilt

--- app/src/main/java/com/iesam/ex_22_23_pmdm_marzo/feature/MainActivity.kt
+++ app/src/main/java/com/iesam/ex_22_23_pmdm_marzo/feature/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.iesam.ex_22_23_pmdm_marzo.R
 import com.iesam.ex_22_23_pmdm_marzo.feature.data.AdoptionsDataRepository
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.adoptions_view)
+        setContentView(R.layout.adoptions_list_view)
 
         val repo = AdoptionsDataRepository()
         repo.getAdoptions()
--- app/src/main/java/com/iesam/ex_22_23_pmdm_marzo/feature/presentation/AdoptionsFragment.kt
+++ app/src/main/java/com/iesam/ex_22_23_pmdm_marzo/feature/presentation/AdoptionsFragment.kt
@@ -1,4 +1,21 @@
 package com.iesam.ex_22_23_pmdm_marzo.feature.presentation
 
-class AdoptionsFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.iesam.ex_22_23_pmdm_marzo.R
+import com.iesam.ex_22_23_pmdm_marzo.app.mock.obtainJsonFromRaw
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AdoptionsFragment: Fragment(R.layout.adoptions_list_view) {
+
+
+     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        val itemView = R.id.name
+         val datos = obtainJsonFromRaw()
+        for (dato in datos){
+
+        }
+    }
 }
